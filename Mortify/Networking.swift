723e9cefa7da1903b66aaece9098262a887a33de
Vlad <vladislav.bok@yandex.ru>
2022-06-21 21:59:46 +0300

    refactor alamofire fun

--- Mortify/Networking.swift
+++ Mortify/Networking.swift
@@ -10,28 +10,19 @@ import Alamofire
 
 public var URL = "https://rickandmortyapi.com/api/character"
 func dataRequest(url: String){
-    AF.request(url, method: .get).responseDecodable(of: Characters.self) { response in
-        print(response)
+    AF.request(url, method: .get)
+        .validate()
+        .responseJSON{ dataResponse in
+            switch dataResponse.result{
+            case .success(_):
+            case .failure(_):
+            
+        }
     }
     
     
     
-//    enum ObtainResult {
-//        case success(response: Characters)
-//        case failure(error: Error)
-//    }
-//
-//    func alamRequest(complition: @escaping (ObtainResult) -> Void){
-//        AF.request(URL).responseDecodable(of: Characters.self) { (responseJSON) in
-//            switch responseJSON.result{
-//            case .success(let JSON):
-//                complition(.success(response: JSON))
-//            case .failure(let error):
-//                complition(.failure(error: error))
-//            }
-//
-//        }
-//    }
+
 }
 
 
