4b45abbdf1249ae65273f138373e913aeff8d74e
Vlad <vladislav.bok@yandex.ru>
2022-07-05 16:56:06 +0300

    Edit architecture, but it isn't working

--- Mortify/View/WebView.swift
+++ Mortify/View/WebView.swift
@@ -5,6 +5,7 @@
 //  Created by Влад Бокин on 22.06.2022.
 //
 
+import Foundation
 import UIKit
 import WebKit
 import Alamofire
@@ -20,7 +21,18 @@ class WebView: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // MARK: - Create webView
+        setup()
+        dataRequest(url: self.urlString)
+        
+        print("dataRequest is loading")
+        // MARK: - Show activity view
+        DispatchQueue.main.async {
+            self.showSpiner()
+        }
+        
+}
+    // MARK: - Create webView
+    func setup() {
         view.addSubview(webView)
         webView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
@@ -29,128 +41,114 @@ class WebView: UIViewController {
             webView.topAnchor.constraint(equalTo: view.topAnchor),
             webView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
         ])
-
-        // MARK: - Show activity view
-        DispatchQueue.main.async {
-            self.showSpiner()
-        }
-
-        // MARK: - Use dispatch group
-        let concurrentQueue = DispatchQueue(label: "ru.mort.concurrent-queue", attributes: .concurrent)
-
-        let group = DispatchGroup()
-
-        let workItem1 = DispatchWorkItem { [self] in
-            func dataRequest(url: String) {
-
-                // MARK: - check 404 error
-                DispatchQueue.main.async {
-                    AF.request(url, method: .get)
-                        .validate()
-                        .response { dataResponse in
-                            switch dataResponse.result {
-                            case .success(let value):
-                                print(value as Any)
-                                self.isError = false
-                                print("Успешно")
-                                self.fetchCountryData()
-                                
-                                
-                            case .failure(let error):
-                                if error.localizedDescription == "Response status code was unacceptable: 404." {
-                                    self.isError = true
-                                    print("Ошибка 404")
-                                    self.fetchCountryData()
-                                    
-                                }
-                            }
-                        }
-                }
-            }
-            dataRequest(url: self.urlString)
-            print("dataRequest is loading")
-            group.leave()
-
-        }
-
-//        let workItem2 = DispatchWorkItem {
-//            self.fetchCountryData()
-//            print("fetchCountryData is loading")
-//            group.leave()
-//
-//        }
-
-        group.enter()
-        concurrentQueue.async(execute: workItem1)
-
-//        group.enter()
-//        concurrentQueue.async(execute: workItem2)
-
-        group.wait()
-
     }
-
+  
     // MARK: - Check country
 
-    // Our target coutries
-    let targetCountries = "Kazakhstan, Turkey, Azerbaijan, Uzbekistan, Ukraine, India, Russia"
-    public var country: String?
-
     func fetchCountryData() {
+        // Our target coutries
+        let targetCountries = "Kazakhstan, Turkey, Azerbaijan, Uzbekistan, Ukraine, India, Russia"
+        var country: String?
+        
         DispatchQueue.main.async {
             MyNetworkService.fetchData(MyNetworkService.checkURL) { [weak self] result in
                 switch result {
                 case .success(let infoDataModel):
-                    self?.country = infoDataModel.country
-                    guard let userCountry = self?.country else { return }
-                    print("User's country: \(userCountry)")
+                    country = infoDataModel.country
+                    guard let userCountry =  country else { return }
+                    Swift.print("User's country: \(userCountry)")
 
                     // Check user country
-                    if self!.targetCountries.contains(userCountry) {
+                    if targetCountries.contains(userCountry) {
                         self?.isError = false
-                        print("User's coutry is acceptable")
+                        Swift.print("User's coutry is acceptable")
                         
                     }
                    
                 case .failure(let err):
                     self?.loadVC()
-                    print(err)
+                    Swift.print(err)
                 }
             }
         }
-        
-
     }
+    
+    func dataRequest(url: String) {
 
-    // MARK: - Load WebView
-    public func loadRequest() {
-        guard let myUrl = NSURL(string: urlString) else { return }
-        let urlRequest = URLRequest(url: myUrl as URL)
-        webView.load(urlRequest)
-        print("WebView loaded")
-
-    }
-
-    // MARK: - Load ErrorScreen
-     func loadErrorScreen() {
-        let errorVC = ErrorInfoVC()
-        self.navigationController?.pushViewController(errorVC, animated: false)
-
-    }
-
-    // MARK: - Check errors
-    public func loadVC() {
-        checkLang()
-        if self.isError == false {
-            self.loadRequest()
-            print("Loading WebView")
+//                // MARK: - check 404 error
+//                DispatchQueue.main.async {
+//                    AF.request(url, method: .get)
+//                        .validate()
+//                        .response { dataResponse in
+//                            switch dataResponse.result {
+//                            case .success(let value):
+//                                print(value as Any)
+//                                self.isError = false
+//                                print("Успешно")
+//                                self.fetchCountryData()
+//
+//                            case .failure(let error):
+//                                if error.localizedDescription == "Response status code was unacceptable: 404." {
+//                                    self.isError = true
+//                                    print("Ошибка 404")
+//                                    self.fetchCountryData()
+//
+//                                }
+//                            }
+//                        }
+//                }
+            
             DispatchQueue.main.async {
-                self.removeSpiner()
+                MyNetworkService.fetchData(url) { [weak self] result in
+                    switch result {
+                    case .success(let value):
+                        Swift.print(value as Any)
+                        self?.isError = false
+                        Swift.print("Успешно")
+                        self?.fetchCountryData()
+
+                    case .failure(let err):
+                        if err.localizedDescription == "Response status code was unacceptable: 404." {
+                            self?.isError = true
+                            Swift.print("Ошибка 404")
+                            self?.fetchCountryData()
+                            
+                        }
+                    }
+                }
             }
-        } else {
-            self.loadErrorScreen()
-            print("Error screen")
+        }
+     
+// MARK: - Load WebView
+func loadRequest() {
+    guard let myUrl = NSURL(string: urlString) else { return }
+    let urlRequest = URLRequest(url: myUrl as URL)
+    webView.load(urlRequest)
+    print("WebView loaded")
+
+}
+
+// MARK: - Load ErrorScreen
+ func loadErrorScreen() {
+    let errorVC = ErrorInfoVC()
+    self.navigationController?.pushViewController(errorVC, animated: false)
+
+}
+
+// MARK: - Check errors
+func loadVC() {
+    checkLang()
+    if self.isError == false {
+      loadRequest()
+        print("Loading WebView")
+        DispatchQueue.main.async {
             self.removeSpiner()
         }
+    } else {
+        loadErrorScreen()
+        print("Error screen")
+        self.removeSpiner()
     }
+}
+
 }
